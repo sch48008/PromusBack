d3be98953fae60e101a983513da1466a2e67fe28
Robert J Schmalz <rschmalz@cox.net>
2016-07-30 01:11:50 +0000

    Friday 7-29 at 6:00 PM

--- common/models/app-user.js
+++ common/models/app-user.js
@@ -1,3 +1,15 @@
 module.exports = function(Appuser) {
-
+    
+    // create an access token after registration of a new user
+    Appuser.observe('after save', function(ctx, next) {
+        if(ctx.isNewInstance === true) {
+            var instance = ctx.instance;
+            instance.createAccessToken(1209600000, function(err, response) {
+                if(err === null) ctx.instance["token"] = response.id;
+                next();
+            });
+        }else {
+             next();
+        }
+    });    
 };
